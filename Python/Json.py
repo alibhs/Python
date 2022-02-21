88fc594499e817a662b8808034e77ea888d62d78
alibhs <77539226+alibhs@users.noreply.github.com>
2022-02-21 04:47:55 +0300

    Delete Python directory

--- Python/Json.py
+++
@@ -1,7 +0,0 @@
-import json
-# Bilgiler={"Ad":"Ali","Soyad":"Bahşi","Yaş":21}
-# print(Bilgiler["Ad"])
-
-Bilgiler="""{"Ad":"Ali","Soyad":"Bahşi","Yaş":21}"""
-bilgiOku = json.loads(Bilgiler)
-print(bilgiOku["Ad"])
--- Python/Requests.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-site = requests.get("https://jsonplaceholder.typicode.com/todos")
-cevap = json.loads(site.text)
-# print(cevap[0])
-# print(cevap[0]["title"])
-for i in cevap:
-    print(i["title"])
